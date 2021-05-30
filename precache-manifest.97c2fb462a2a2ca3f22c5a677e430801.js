32476f63b306cbf76de3d342e21f5014f882f794
victor <victorarytionowmail@gmail.com>
2021-05-30 18:00:42 +0300

    Updates

---
+++ precache-manifest.97c2fb462a2a2ca3f22c5a677e430801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba996fe55da2846b9d763d521587498",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd386750b1f32932201a",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "8febe959d21699fd1ef2",
+    "url": "./static/css/main.43abedb4.chunk.css"
+  },
+  {
+    "revision": "fd386750b1f32932201a",
+    "url": "./static/js/2.484eecf1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.484eecf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8febe959d21699fd1ef2",
+    "url": "./static/js/main.868a488e.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.97c2fb462a2a2ca3f22c5a677e430801.js"
 );
 
 self.addEventListener('message', (event) => {
